a1ea1b1e8e361489ce795e3999020664c6245aa2
linabdn <u36lm24@abdn.ac.uk>
2025-10-19 15:16:04 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# Report 
+
+Web Development class April 2025
+
+
+## Making of the design 
+
+I designed each page one by one with the intention of making a minimalist though colorful website.
+Intention of using rem as much as possible, as opposed to my first project to ensure better responsiveness.
+
+
+## Making of the home page 
+
+It is a simple layout with a navbar with a logo and a link to the other two pages. I put a small presentation of myself with a portrait of Van Gogh rather than a picture. It was a placeholder at first but it fit very well so I left it. 
+I thought the page lacked movement/animation as no scrolling is needed on large screens. Therefore I added moving flowers in the background. It wasn't too challenching (animation in css and random position with javascript)
+
+
+## Making of the Info/More page 
+I reused the layout of the History page of my first website. It is however very different. Each image is a png flower that inverts when hovering. The insides of each lightbox is much more elaborated. Only the concept is reused. 
+I find the page has a rather original and unique look. 
+Content development was rather easy and mainly based on my resume. 
+
+noticed an issue with the scrollbar in the lightbox.
+when wide screen going into smallscreen, the scrollbar has a bug: it either doesnt appear or it appears but doesn't work unless the page is refreshed. this is a small responsiveness issue i wasn't able to fix. 
+
+
+## Making of the chat page
+
+Used the given code and added styles to it. From there, I only needed to add the listening features on the javascript and add styles to them. 
+
+
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+//most of the original code was kept intact. comments only where modifications were made or parts added
+
 const express = require("express");
 const socket = require("socket.io");
 
-// App setup
 const PORT = 5000;
 const app = express();
 const server = app.listen(PORT, function () {
@@ -9,13 +10,10 @@ const server = app.listen(PORT, function () {
 
 });
 
-// Static files
 app.use(express.static("public"));
 
-// Socket setup
 const io = socket(server);
 
-//we use a set to store users, sets objects are for unique values of any type
 const activeUsers = new Set();
 
 io.on("connection", function (socket) {
@@ -24,18 +22,29 @@ io.on("connection", function (socket) {
   socket.on("new user", function (data) {
     socket.userId = data;
     activeUsers.add(data);
-    //... is the the spread operator, adds to the set while retaining what was in there already
     io.emit("new user", [...activeUsers]);
+    //notifying all the other clients when a user joins
     socket.broadcast.emit("user joined", socket.userId); 
   });
 
   socket.on("disconnect", function () {
       activeUsers.delete(socket.userId);
+      //notifying all clients when a user leaves
       io.emit("user disconnected", socket.userId);
     });
 
     socket.on("chat message", function (data) {
       io.emit("chat message", data);
+  });
+
+  //notifying all other clients when this user is typing
+  socket.on("typing", function(username) {
+    socket.broadcast.emit("typing", username);
+  });
+
+  //stopping the notification when the user stops typing or sent msg
+  socket.on("stop typing", function(username) {
+    socket.broadcast.emit("stop typing", username);
   });
 
 });
